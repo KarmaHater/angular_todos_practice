b88944e78924b95e0acdd60314bf887425817f12
Adnard-kod <andra.lally@gmail.com>
2014-12-18 11:21:03 -0800

    sent errors messages to the front end

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,25 @@
 var app = angular.module('todo', []);
     app.controller('todoController', ['$http', '$scope', function($http, $scope) {
+          $scope.errors = []
           $http.get('/todos')
           .success(function(data){
             $scope.todos = data
           })
-          .error(function(){
-            alert(" shit's weak")
+          .error(function(xrh){
+
           })
           $scope.addTodo = function(){
             $http.post('/todos', {todo: {title: $scope.formText}})
             .success(function(data){
-              console.log(data)
-              $scope.todos.push(data)
+              if (data.errors) {
+                console.log(data.errors)
+              }else{
+                $scope.todos.push(data)
+              };
               $scope.formText = "";
+            })
+            .error(function(xrh){
+              $scope.errors = xrh.responseJSON.errors
             })
           };
         }]);
